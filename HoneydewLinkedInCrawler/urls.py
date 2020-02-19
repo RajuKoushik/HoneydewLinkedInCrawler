138c004f458908cf0a1335e46a930fa490347b2e
RajuKoushik <g.rajukoushik@gmail.com>
2020-02-18 23:21:12 -0700

    Updated the crawler api.

--- HoneydewLinkedInCrawler/urls.py
+++ HoneydewLinkedInCrawler/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from crawler.views import crawler
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('crawl/', crawler),
+
 ]
--- crawler/views.py
+++ crawler/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import json
+
+import requests
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+
+
+@csrf_exempt
+def crawler(request):
+    print(request.GET)
+
+    # making a request to a google custom search engine
+    custom_search_engine_url = "https://www.googleapis.com/customsearch/v1"
+
+    PARAMS = {'key': 'AIzaSyByUxDR0YO701YOETlSJZn6bfFNWIjtQBM', 'cx': '009462381166450434430:ecyvn9zudgu',
+              'q': request.GET['query']}
+
+    # sending get request and saving the response as response object
+    r = requests.get(url=custom_search_engine_url, params=PARAMS)
+
+    # extracting data in json format
+    custom_search_engine_data = r.json()
+
+    print(custom_search_engine_data)
+
+    return HttpResponse(
+        json.dumps(
+            {
+                'current_user': 1,
+                'post_name': 1,
+                'post_content': 1,
+
+            }
+        )
+    )
