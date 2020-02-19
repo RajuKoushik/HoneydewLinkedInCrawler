d1725d5391654ae25681d0d6b89b74c650c9309a
RajuKoushik <g.rajukoushik@gmail.com>
2020-02-18 22:56:17 -0700

    Updated models.

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,32 @@
+# Create your models here.
+
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.TextField(max_length=500, blank=True)
+    linkedin_url = models.TextField(max_length=1500, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Certifications(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    certification_name = models.TextField(max_length=500, blank=True)
+    time_stamp = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.certification_name
+
+
+class CurrentJob(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    job_name = models.TextField(max_length=500, blank=True)
+    time_stamp = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.certification_name
