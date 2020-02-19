150fe407e74ff3aa95174d871826a23fa40c190a
RajuKoushik <g.rajukoushik@gmail.com>
2020-02-19 12:39:35 -0700

    Updated the crawler api.

--- crawler/views.py
+++ crawler/views.py
@@ -6,12 +6,18 @@ from bs4 import BeautifulSoup
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from fake_useragent import UserAgent
+from linkedin_scraper import Person, actions
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 ua = UserAgent()  # From here we generate a random user agent
 proxies = []
 
 
 def get_proxies():
+    proxy = []
     proxies_req = Request('https://www.sslproxies.org/')
     proxies_req.add_header('User-Agent', ua.random)
     proxies_doc = urlopen(proxies_req).read().decode('utf8')
@@ -21,6 +27,10 @@ def get_proxies():
 
     # Save proxies in the array
     for row in proxies_table.tbody.find_all('tr'):
+        a = row.find_all('td')[0].string + row.find_all('td')[1].string
+
+        proxy.append({'https': a})
+
         proxies.append({
             'ip': row.find_all('td')[0].string,
             'port': row.find_all('td')[1].string
@@ -31,6 +41,12 @@ def get_proxies():
 
 @csrf_exempt
 def crawler(request):
+    email = "g.rajukoushik@gmail.com"
+    password = "rituraja"
+    actions.login(driver, email, password)
+
+    name_list = []
+
     print(request.GET)
 
     proxy_list = get_proxies()
@@ -59,6 +75,17 @@ def crawler(request):
             # linkedin_url_data = custom_search_engine_data['items']
             #
             # print(custom_search_engine_data['items'][0]['link'])
+
+    for i in linkedin_url_list:
+
+        try:
+            # if email and password isnt given, it'll prompt in terminal
+            person = Person(i, driver=driver)
+            name_list.append(person)
+        except:
+            pass
+
+    print(name_list)
 
     return HttpResponse(
         json.dumps(
