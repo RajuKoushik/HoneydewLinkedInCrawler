be0857a30d3b148d53c7da13fe3b211825b125c9
RajuKoushik <g.rajukoushik@gmail.com>
2020-03-03 18:07:00 -0700

    Updated and added models.py

--- crawler/models.py
+++ crawler/models.py
@@ -6,27 +6,10 @@ from django.db import models
 class Profile(models.Model):
     name = models.TextField(max_length=500, blank=True)
     linkedin_url = models.TextField(max_length=1500, blank=True)
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+    certifications = models.TextField(blank=True)
+    current_job = models.TextField(max_length=500, blank=True)
+    companies = models.TextField(blank=True)
+    is_updated = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
-
-
-class Certifications(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    certification_name = models.TextField(max_length=500, blank=True)
-    time_stamp = models.DateField(null=True, blank=True)
-
-    def __str__(self):
-        return self.certification_name
-
-
-class CurrentJob(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    job_name = models.TextField(max_length=500, blank=True)
-    time_stamp = models.DateField(null=True, blank=True)
-
-    def __str__(self):
-        return self.certification_name
--- crawler/views.py
+++ crawler/views.py
@@ -1,14 +1,18 @@
 import json
+import time
 from urllib.request import Request, urlopen
 
 import requests
 from bs4 import BeautifulSoup
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from fake_useragent import UserAgent
-from linkedin_scraper import Person, actions
+from linkedin_scraper import actions
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+
+from .models import Profile
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -39,6 +43,74 @@ def get_proxies():
     return proxies
 
 
+def get_individual_result_from_linkedin(url, driver):
+    individual_user_data = {}
+    individual_user_data[url] = {}
+    companies = []
+    certifications = []
+
+    driver.get(url)
+    # Depends on how fast your server loads javascript and htmls
+    # This code is to load entire html code, not only source code
+    time.sleep(5)
+    body = driver.find_element_by_tag_name("body")
+    last_height = driver.execute_script(
+        "return document.body.scrollHeight")
+    while True:
+        body.send_keys(Keys.PAGE_DOWN)
+        time.sleep(5)
+        new_height = driver.execute_script(
+            "return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        else:
+            last_height = new_height
+
+    source = driver.page_source
+    soup = BeautifulSoup(source, 'lxml')
+
+    # Find all profile sections, It will find all headings like Experiences, Companies, Licenses etc
+    all_divs = soup.findAll(class_="pv-profile-section-pager")
+
+    for div in all_divs:
+        headers = div.findAll('h2')
+        for h in headers:
+            if h.has_attr('class') and h['class'][0] == 'pv-profile-section__card-heading':
+                # print(h.get_text().strip())
+                extractedText = h.get_text().strip()
+                if (extractedText == 'Experience'):
+                    insideDivs = div.findAll('div')
+                    for d in insideDivs:
+                        # pv-entity__summary-info pv-entity__summary-info--background-section
+                        if d.has_attr('class') and d['class'][0] == 'pv-entity__summary-info' and d['class'][
+                            1] == 'pv-entity__summary-info--background-section':
+                            pAll = d.findAll('p')
+
+                            for p in pAll:
+                                if p.get_text().strip() != 'Company Name':
+                                    companies.append(p.get_text().strip())
+
+                if (extractedText == 'Licenses & Certifications'):
+                    # <h3 class="t-16 t-bold">Epic Beaker Anatomic Pathology</h3>
+                    insideHeaders = div.findAll('h3')
+                    for h3 in insideHeaders:
+                        if h3.has_attr('class') and h3['class'][0] == 't-16' and h3['class'][1] == 't-bold':
+                            # print(h3.get_text())
+                            certifications.append(h3.get_text().strip())
+
+    try:
+
+        individual_user_data[url]['current_employer'] = companies[0]
+
+    except:
+        individual_user_data[url]['current_employer'] = 'None'
+
+    individual_user_data[url]['companies'] = companies
+    individual_user_data[url]['certifications'] = certifications
+    # find name, current position,
+    return individual_user_data
+
+
 @csrf_exempt
 def crawler(request):
     email = "g.rajukoushik@gmail.com"
@@ -49,9 +121,9 @@ def crawler(request):
 
     print(request.GET)
 
-    proxy_list = get_proxies()
+    # proxy_list = get_proxies()
 
-    print(proxy_list)
+    # print(proxy_list)
 
     # making a request to a google custom search engine
     custom_search_engine_url = "https://www.googleapis.com/customsearch/v1"
@@ -76,24 +148,38 @@ def crawler(request):
             #
             # print(custom_search_engine_data['items'][0]['link'])
 
-    for i in linkedin_url_list:
+    print(linkedin_url_list)
 
-        try:
-            # if email and password isnt given, it'll prompt in terminal
-            person = Person(i, driver=driver)
-            name_list.append(person)
-        except:
-            pass
+    dicty = {}
 
-    print(name_list)
+    for linkedin in linkedin_url_list:
+        temp = get_individual_result_from_linkedin(linkedin, driver)
+        dicty[linkedin] = get_individual_result_from_linkedin(linkedin, driver)
+        print(temp)
 
-    return HttpResponse(
-        json.dumps(
-            {
-                'linkedin_url-list': str(linkedin_url_list),
-                'post_name': 1,
-                'post_content': 1,
+        profile_model = Profile()
 
-            }
-        )
-    )
+        URL = list(temp.keys())[0]
+        profile_model.name = URL.split("/")[-1]
+        profile_model.linkedin_url = URL
+        profile_model.companies = json.dumps(temp[URL]['companies'])
+        profile_model.current_job = temp[URL]['current_employer']
+        profile_model.certifications = json.dumps(temp[URL]['certifications'])
+        profile_model.is_updated = False
+        profile_model.save()
+    p = Profile()
+    profile_all = p.objects.all()
+
+    return render(request, 'index.html', {'profile': profile_all, })
+
+    # return HttpResponse(
+    #     json.dumps(
+    #         {
+    #             'linkedin_url-list': str(linkedin_url_list),
+    #             'post_name': str(name_list),
+    #             'post_content': 1,
+    #             'dict': dicty
+    #
+    #         }
+    #     )
+    # )
